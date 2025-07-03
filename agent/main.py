9227b89c0f34e58f6c410a384d529048c83d8894
Letsktor <vajasmate2013@gmail.com>
2025-07-03 14:39:18 +0200

    Added calculator

--- agent/main.py
+++ agent/main.py
@@ -2,22 +2,34 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 def main():
-    if len(sys.argv)==2:
-        load_dotenv()
-        api_key=os.environ.get("GEMINI_API_KEY")
-        client=genai.Client(api_key=api_key)
-        prompt=sys.argv[1]
-        response=client.models.generate_content(model="gemini-2.0-flash-001",contents=prompt)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    if len(sys.argv) == 2:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=messages)
         print(response.text)
+
+    elif len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=messages)
+        print(response.text)
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(
+            f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
         raise Exception("Prompt wasn't provided!")
         exit(1)
+
 
 if __name__ == "__main__":
     main()
